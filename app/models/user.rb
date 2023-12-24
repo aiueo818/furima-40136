72d56b6333837d62bedc0ee42a0bd595b260342a
aiueo818 <ioririko104@gmail.com>
2023-12-24 16:54:38 +0900

    バリデーションの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,last_name,first_name,last_name_reading,last_name_reading,birth, presence: true
 end
+
+
+
+
+      t.date :birth,                null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_reading,  null: false
+      t.string :last_name_reading, null: false
