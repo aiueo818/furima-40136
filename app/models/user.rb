b9260adafe99f45eef54c1751a5d2e98a413d530
aiueo818 <ioririko104@gmail.com>
2023-12-25 16:49:39 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,last_name,first_name,last_name_reading,last_name_reading,birth, presence: true
+  validates :nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birth, presence: true
+  validates :last_name, :first_name, format: { with: /\A[\p{hiragana}\p{katakana}\p{blank}ー－]+\z/, message: "は全角で入力してください" }
+validates :last_name_reading, :first_name_reading, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
 end
-
-
-
-
-      t.date :birth,                null: false
-      t.string :last_name,          null: false
-      t.string :first_name,         null: false
-      t.string :last_name_reading,  null: false
-      t.string :last_name_reading, null: false
