a8188e0a12881113be84ee9cf69d4f6367e8047f
aiueo818 <ioririko104@gmail.com>
2023-12-29 17:40:39 +0900

    商品情報編集機能
    # What
    商品情報編集機能の実装
    # Why
    最終課題において順番に実装していくため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   
   def index
     @items = Item.all
@@ -20,6 +20,25 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  if @item.update(item_params)
+    redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
