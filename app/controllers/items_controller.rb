f2c252928295df4343d362f83daad601d5341bce
aiueo818 <ioririko104@gmail.com>
2023-12-29 18:36:29 +0900

    未ログイン時のページ遷移
    # What
    未ログイン時のページ遷移を制限
    # Why
    未ログインユーザーが削除するのを防ぐため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    unless user_signed_in?
+      redirect_to action: :index
+    end
     @item.destroy
     redirect_to root_path
   end
