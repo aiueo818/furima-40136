d0b109626e38e9b459e499358156051f51907428
aiueo818 <ioririko104@gmail.com>
2023-12-29 17:59:24 +0900

    商品情報編集機能
    # What
    同じ記述がある箇所をbeforeアクションでまとめる
    # Why
    可読性向上のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_user, only: [:edit, :update]
   
   def index
     @items = Item.all
@@ -19,21 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
   if @item.update(item_params)
     redirect_to item_path
     else
@@ -47,4 +40,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name,:explanation,:category_id, :status_id, :postage_id, :shipping_id,:number_of_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_user
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+  
 end
