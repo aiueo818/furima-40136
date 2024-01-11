2bf34e9eaa2f58c3f5a8bc40eedc1a2663c11cc4
aiueo818 <ioririko104@gmail.com>
2024-01-11 15:48:18 +0900

    販売手数料や利益の反映
    # What
    手数料と利益の計算の読み込み部分の修正
    # Why
    エラーハンドリングによって出品ページに戻った際に、手数料や利益の計算が反映されないため

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -11,7 +11,9 @@ window.addEventListener('turbo:load', () => {
     const profitDom = document.getElementById("profit");
     const salesProfit = inputValue - feeAmount;
     profitDom.innerHTML = salesProfit;
-
-});
-
   });
+};
+
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
